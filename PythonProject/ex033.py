38b83a1f5ef51e8f2c0e03c89306cd7f5ee68afe
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-19 11:43:14 -0300

    ex033 Professor Guanabara

--- PythonProject/ex033.py
+++ PythonProject/ex033.py
@@ -15,5 +15,23 @@ if m < n and m > o:
 else:
     print('O menor número é {}'.format(n))
 
+#professor guanabara
+
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor: '))
+c = int(input('Terceiro valor: '))
+ menor = a
+ if b<a and b<c:
+     menor = b
+if c<a and c<b:
+    menor = c
+# Verificando maior
+maior =  a
+if b>a and b>c:
+    maior = b
+if c>a and c>b:
+    maior = c
+print('O menor valor foi {}'.format(menor))
+print('O maior valor foi {}'.format(maior))
 
 
