358f29d3267615bee28964aeb75d104217dfbb22
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-19 15:20:20 -0300

    ex035 professor

--- PythonProject/ex035.py
+++ PythonProject/ex035.py
@@ -2,13 +2,24 @@ n = float(input('Digite um segmento: '))
 m = float(input('Digite outro segmento : '))
 o = float(input('Digite um terceiro: '))
 
-if n < m + o and m < n + o and o < n + o:
+if n < m + o and m < n + o and o < n + m:
     print('Você tem um triângulo')
 else:
     print('Você não tem um triângulo')
 
 #professor guanabara
+print('-= * 20')
+print('Analisando um triângulo...')
+print('-=-' * 20)
 
 
+r1 = float(input('Digite um segmento: '))
+r2 = float(input('Digite outro segmento : '))
+r3 = float(input('Digite um terceiro: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Você tem um triângulo')
+else:
+    print('Você não tem um triângulo')
 
 
