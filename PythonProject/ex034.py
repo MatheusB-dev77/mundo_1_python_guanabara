5cdbbe49cd2cefac9cf8f8fab8349af0e95a48aa
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-19 12:40:42 -0300

    ex034

---
+++ PythonProject/ex034.py
@@ -0,0 +1,9 @@
+n = float(input('Qual o salário do funcionário'))
+
+if n <= 5000:
+    print('Quem ganha R${} passa a ganhar R${:.2f} agora.'.format(n, (n*1.15)))
+elif n <= 7000:
+     print('Quem ganha R${} passa a ganhar R${:.2f}'.format(n,(n * 1.12)))
+else :
+    print('Quem ganha R${} passa a ganhar R${:.2f}'.format(n, (n * 1.10)))
+
