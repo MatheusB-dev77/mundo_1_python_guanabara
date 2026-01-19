237bb1f00d91b43fbf22641bff3015ee26c9dc75
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-18 22:00:34 -0300

    ex032 professor

--- ex032.py
+++ ex032.py
@@ -4,3 +4,13 @@ if ano == 0:
     print('Seu ano {} é bissexto'.format(ano))
 else:
     print('Seu ano não é bissexto')
+
+#professor guanabara
+from datetime import date
+ano = int(input('Que ano quer analisar'))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print ('O ano {} é Bissexto'.format(ano))
+else:
+    print('O ano {} não é Bissexto'.format(ano))
