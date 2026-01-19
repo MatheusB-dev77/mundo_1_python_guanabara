7ed2cba6556fda291fcd6b9a6b0334b95e3a53f5
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-19 13:43:00 -0300

    ex034 professor

--- PythonProject/ex034.py
+++ PythonProject/ex034.py
@@ -7,3 +7,13 @@ elif n <= 7000:
 else :
     print('Quem ganha R${} passa a ganhar R${:.2f}'.format(n, (n * 1.10)))
 
+#professor guanabara
+
+salario = float(input('Qual é o salário do funcionário'))
+if salario <=1250:
+    novo = salario + (salario * 15 / 100)
+else:
+    novo = salario + (salario * 10 / 100)
+print('Quem ganhava R$ {:.2f} passa a ganhar r${:.2f} agora}'.format(salario, novo))
+
+
