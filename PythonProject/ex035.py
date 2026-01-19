f271c9959572f4864425fd8ad2c853842e683c3f
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-19 15:12:47 -0300

    ex035

---
+++ PythonProject/ex035.py
@@ -0,0 +1,14 @@
+n = float(input('Digite um segmento: '))
+m = float(input('Digite outro segmento : '))
+o = float(input('Digite um terceiro: '))
+
+if n < m + o and m < n + o and o < n + o:
+    print('Você tem um triângulo')
+else:
+    print('Você não tem um triângulo')
+
+#professor guanabara
+
+
+
+
