6e0abde40a7f7883c8c0600f904937ccd8de1068
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-20 17:16:30 -0300

    Cores_python

--- PythonProject/aula11.py
+++ PythonProject/aula11.py
@@ -24,3 +24,4 @@ cores = {'limpa':'\033[m',
          'amarelo':'\033[33m',
          'pretoebranco':'\033[7;30m'}
 print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format(cores['pretoebranco'], nome, cores['limpa']))
+
